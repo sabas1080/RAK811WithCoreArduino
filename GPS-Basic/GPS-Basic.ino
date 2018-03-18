1d0d42b6d8823f78138d045a6e026042064c9fcb
sabas1080 <s@theinventorhouse.org>
2018-03-18 14:59:38 -0600

    Example for GPS MAX7Q RAK811 Tracker Board

---
+++ GPS-Basic/GPS-Basic.ino
@@ -0,0 +1,94 @@
+/*
+ * Basic Example GPS for RAK811 Tracker Board
+ * Basic demo for GPS MAX7Q readings from TinyGPS++
+ * Arduino Core: https://github.com/sabas1080/Arduino_Core_STM32
+ * Autor: Andrés Sabas
+ * Date: 17 March 2018
+ * for Electronic Cats @ https://electroniccats.com 
+ */
+#include <TinyGPS++.h>
+ 
+HardwareSerial Serial3(GPS_UART_RX, GPS_UART_TX);
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+void setup()
+{ 
+  pinMode(GPS_POWER_ON_PIN, OUTPUT);
+  digitalWrite(GPS_POWER_ON_PIN, HIGH);
+  
+  Serial.begin(115200);
+  Serial3.begin(9600);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial3.available() > 0)
+    if (gps.encode(Serial3.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
